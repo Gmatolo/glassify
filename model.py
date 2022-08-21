c5f45bcf3a0e412801e89326389515d05601d6cd
gmatolo <geraldmuli7@gmail.com>
2022-08-21 07:27:27 +0300

    Updating glass model

---
+++ model.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import joblib
+
+dataset = pd.read_csv("glass.csv")
+
+
+#independent variables
+X=dataset.iloc[:,:-1] 
+#dependent variables
+y=dataset.iloc[:,9]
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 42)
+
+from sklearn.preprocessing import StandardScaler
+sc_x=StandardScaler()
+X_train = sc_x.fit_transform(X_train)
+X_test = sc_x.transform(X_test)
+
+from sklearn.ensemble import RandomForestClassifier
+
+cls = RandomForestClassifier(criterion='entropy', n_estimators=300, random_state=42)
+
+cls.fit(X_train, y_train)
+
+
+print('ACCURACY is ', cls.score(X_test, y_test)*100, '%')
+
+#save model to disk
+filename = "finalized_model.sav"
+joblib.dump(cls, filename)
+
+
+
